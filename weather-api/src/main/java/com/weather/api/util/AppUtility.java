adba73b8c006b62d9cf8858cdae98ac22471180a
vivek22117 <vivekmishra22117@gmail.com>
2021-04-17 21:49:28 +0530

    adding utility class for constants

--- weather-api/src/main/java/com/weather/api/util/AppUtility.java
+++ weather-api/src/main/java/com/weather/api/util/AppUtility.java
@@ -1,4 +1,9 @@
 package com.weather.api.util;
 
 public class AppUtility {
+
+    public static final String API_KEY = "1236405a5eb81bcefbf9ae0e9b9b226c";
+
+    public static final String WEATHER_ROOT_URL = "http://api.openweathermap.org/data/2.5/weather";
+
 }
