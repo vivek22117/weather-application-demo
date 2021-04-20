73e3db8f3d756d8997a9ffee35f277487fbd9fb1
vivek22117 <vivekmishra22117@gmail.com>
2021-04-20 16:00:24 +0530

    custom response for login api

---
+++ weather-api/src/main/java/com/weather/api/model/AuthenticationResponse.java
@@ -0,0 +1,18 @@
+package com.weather.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthenticationResponse {
+    private String authenticationToken;
+    private Instant expiresAt;
+    private String username;
+}
