11b56d29af50d3efd91a4a0193f9a4e2f8c880c2
vivek22117 <vivekmishra22117@gmail.com>
2021-04-21 10:26:42 +0530

    updating apputility class

--- weather-api/src/main/java/com/weather/api/util/AppUtility.java
+++ weather-api/src/main/java/com/weather/api/util/AppUtility.java
@@ -4,6 +4,7 @@ public class AppUtility {
 
     public static final String API_KEY = "1236405a5eb81bcefbf9ae0e9b9b226c";
 
-    public static final String WEATHER_ROOT_URL = "http://api.openweathermap.org/data/2.5/weather";
+    public static final String WEATHER_ROOT_URL = "http://api.openweathermap.org/data/2.5/weather?";
+    public static final long JWT_TOKEN_VALIDITY = 3600;
 
 }
