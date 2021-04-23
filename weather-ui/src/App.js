7144672cd1fe4e378707517e4f1078bc9d10a7c4
vivek22117 <vivekmishra22117@gmail.com>
2021-04-23 12:30:39 +0530

    updating App js file for login and singup

---
+++ weather-ui/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import SignInOutContainer from "./container";
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <SignInOutContainer/>
+      </div>
+    )
+  }
+}
+
+export default App;
