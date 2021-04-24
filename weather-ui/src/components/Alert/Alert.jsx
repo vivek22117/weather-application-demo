7f17fbdc82f3559a2ede59613a1eddfbc6628981
vivek22117 <vivekmishra22117@gmail.com>
2021-04-24 19:07:57 +0530

    updating Alert component

--- weather-ui/src/components/Alert/Alert.jsx
+++ weather-ui/src/components/Alert/Alert.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
 import styles from './Alert.module.css'
+import {useState, useEffect} from "react";
 
 const Alert = (props) => {
-  console.log(props)
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    const timeId = setTimeout(() => {
+      setShow(false);
+    }, 5000);
+
+    return () => clearTimeout(timeId);
+  }, []);
+
+  console.log(show);
+
   return (
+    show &&
     <div className={styles.alert}
          style={props.alert.isDashboard ? {position: "absolute"} : {position: "static", fontSize: ""}}>
       <h3 style={props.alert.isDashboard ? {fontSize: "1.8rem"} : {fontSize: "1rem"}}>{props.alert.alert}</h3>
