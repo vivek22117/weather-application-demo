240216fe11332bcadc345f822608ff4536adaedf
vivek22117 <vivekmishra22117@gmail.com>
2021-04-25 15:38:38 +0530

    updating App js file for logout funcationality

--- weather-ui/src/App.js
+++ weather-ui/src/App.js
@@ -1,14 +1,77 @@
 import React from "react";
 
-import SignInOutContainer from "./container";
+import {Dashboard, Home, Logout, Navbar, SignInOutContainer} from "./components";
+import {grey} from "@material-ui/core/colors";
+import {Container} from "@material-ui/core";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {getCurrentUser, removeUserSession} from "./Utils/Common";
+
+const appStyles = {width: "100vw", height: "100vh", backgroundColor: grey};
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+      currentUser: undefined,
+      showAdminPage: false
+    };
+  }
+
+  componentDidMount() {
+    const user = getCurrentUser();
+
+    console.log(user);
+
+    if (user) {
+      this.setUserState(user, true, true);
+    }
+  };
+
+
+  setUserState = (user, isAuthenticate, showAdminPage) => {
+    this.setState({
+      isAuthenticated: isAuthenticate,
+      currentUser: user,
+      showAdminPage: showAdminPage
+    })
+  };
+
+
   render() {
+
     return (
-      <div>
-        <SignInOutContainer/>
-      </div>
+      <BrowserRouter>
+        <Container style={appStyles} disableGutters>
+          <Navbar userState={this.state}
+                  setUserState={this.setUserState}
+          />
+          <Switch>
+            <Route exact
+                   path={"/"}
+                   render={props => (
+                     <Home isAuthenticated={this.state.isAuthenticated}/>
+                   )}
+            />
+            <Route exact
+                   path={"/manage-user"}
+                   render={props => (
+                     <SignInOutContainer {...props}
+                                         isAuthenticated={this.state.isAuthenticated}
+                                         setUserState={this.setUserState}
+                     />
+                   )}
+            />
+            <Route exact
+                   path={"/dashboard"}
+                   render={props => (
+                     <Dashboard {...props} isAuthenticated={this.state.isAuthenticated}/>
+                   )}
+            />
+          </Switch>
+        </Container>
+      </BrowserRouter>
     )
   }
 }
