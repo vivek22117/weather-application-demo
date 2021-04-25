035d5940688d44dcef1a69092c457204ccce4d6a
vivek22117 <vivekmishra22117@gmail.com>
2021-04-25 18:11:53 +0530

    Adding Entrypoint exception and access denied handler

---
+++ weather-api/src/main/java/com/weather/api/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.weather.api.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException e) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.getWriter().write("Insufficient authentication details");
+    }
+}
