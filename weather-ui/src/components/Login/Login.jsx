9dc98df5bb14e3b7d73296684a0bcdf4bf544560
vivek22117 <vivekmishra22117@gmail.com>
2021-04-25 22:24:41 +0530

    updaitng alert component

--- weather-ui/src/components/Login/Login.jsx
+++ weather-ui/src/components/Login/Login.jsx
@@ -1,14 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
+import {useHistory} from 'react-router-dom';
 import {Avatar, Button, FormControlLabel, Grid, Link, Paper, TextField, Typography} from '@material-ui/core';
 import LockOutLinedIcon from '@material-ui/icons/LockOutlined';
 import Checkbox from '@material-ui/core/Checkbox';
-import {Formik, Form} from "formik";
+import axios from "axios";
+import {setUserSession} from "../../Utils/Common";
 
 import styles from './Login.module.css'
+import {Alert} from "../index";
 
-const Login = ({handleChange}) => {
+const Login = ({handleChange, setUserState}) => {
+  const history = useHistory();
 
-  const buttonStyle={margin: "8px 0"}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [alertData, setAlert] = useState({alert: "", isDashboard: false});
+
+  const buttonStyle = {margin: "8px 0"}
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = {
+      username: username,
+      password: password
+    };
+    console.log(data);
+
+    if (data.password !== "" || data.username !== "") {
+      axios.post('api/auth/login', data)
+        .then(res => {
+          console.log(res);
+          if (!res.data.authenticationToken) {
+            return setAlert({alert: "Authentication failed!", isDashboard: false});
+          }
+          setUserSession(res.data.authenticationToken, res.data.username, res.data.isAuthenticated);
+          setUserState(res.data.username, res.data.isAuthenticated, true);
+          history.push('/')
+        })
+        .catch(err => {
+          console.log(err);
+          setAlert({alert: err.response.data, isDashboard: false});
+
+        });
+    } else {
+      setAlert({alert: "Please provide valid credentials!", isDashboard: false});
+    }
+  };
 
   return (
 
@@ -18,24 +56,28 @@ const Login = ({handleChange}) => {
           <Avatar className={styles.avatarcontainer}><LockOutLinedIcon/></Avatar>
           <h2>Sign In</h2>
         </Grid>
-       <Formik>
-         {(props) => (
-           <Form>
-             <TextField label='Username' placeholder='Enter username' fullWidth required/>
-             <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
-             <FormControlLabel
-               control={
-                 <Checkbox
-                   name="checkedB"
-                   color="primary"
-                 />
-               }
-               label="Remember me"
-             />
-             <Button type={"submit"} color={"primary"} variant={"contained"} style={buttonStyle} fullWidth>SIGN IN</Button>
-           </Form>
-         )}
-       </Formik>
+        <TextField label='Username'
+                   placeholder='Enter username' fullWidth
+                   onChange={(event) =>
+                     setUsername(event.target.value)
+                   } required/>
+        <TextField label='Password'
+                   placeholder='Enter password' type='password' fullWidth
+                   onChange={(event) =>
+                     setPassword(event.target.value)
+                   }
+                   required/>
+        <FormControlLabel
+          control={
+            <Checkbox
+              name="checkedB"
+              color="primary"
+            />
+          }
+          label="Remember me"
+        />
+        <Button type={"submit"} color={"primary"} variant={"contained"} style={buttonStyle}
+                onClick={(event) => onSubmit(event)} fullWidth>SIGN IN</Button>
 
         <Typography>
           <Link href="#">
@@ -44,14 +86,14 @@ const Login = ({handleChange}) => {
         </Typography>
         <Typography> Do you have an account?
           <Link href="#" onClick={() => handleChange("event", 1)}>
-             Sign Up
+            Sign Up
           </Link>
         </Typography>
-
+        {alertData.alert !== "" && <Alert alert={alertData} alertStatus={false}/>}
       </Paper>
     </Grid>
 
-  )
-}
+  );
+};
 
 export default Login;
