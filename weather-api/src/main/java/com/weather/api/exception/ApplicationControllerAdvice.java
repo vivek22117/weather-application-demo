92b894b8af2e1850d91316d6df0ba963af2dd250
vivek22117 <vivekmishra22117@gmail.com>
2021-04-26 14:35:07 +0530

    removing unwanted imports from controller advice

--- weather-api/src/main/java/com/weather/api/exception/ApplicationControllerAdvice.java
+++ weather-api/src/main/java/com/weather/api/exception/ApplicationControllerAdvice.java
@@ -5,16 +5,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
 import java.security.InvalidParameterException;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class ApplicationControllerAdvice extends ResponseEntityExceptionHandler {
 
@@ -27,24 +27,24 @@ public class ApplicationControllerAdvice extends ResponseEntityExceptionHandler 
     }
 
     @ExceptionHandler(InvalidParameterException.class)
-    public ResponseEntity<String> invalidRegistrationInput(InvalidParameterException invalidParameterException) {
+    public ResponseEntity<Object> invalidRegistrationInput(InvalidParameterException invalidParameterException) {
         log.error("Invalid registration parameters, pleas re-check");
         return new ResponseEntity<>(invalidParameterException.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserAuthenticationException.class)
-    public ResponseEntity<String> handleAuthenticationError(UserAuthenticationException authenticationException) {
+    public ResponseEntity<Object> handleAuthenticationError(UserAuthenticationException authenticationException) {
         log.error("Login authentication failed, please recheck credentials");
         return new ResponseEntity<>(authenticationException.getMessage(), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<String> handleBusinessException(BusinessException exception) {
+    public ResponseEntity<Object> handleBusinessException(BusinessException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NO_CONTENT);
     }
 
     @ExceptionHandler(WeatherDataNoFoundException.class)
-    public ResponseEntity<String> handleNoDataFound(WeatherDataNoFoundException exception) {
+    public ResponseEntity<Object> handleNoDataFound(WeatherDataNoFoundException exception) {
         log.error("No data found");
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
@@ -58,8 +58,9 @@ public class ApplicationControllerAdvice extends ResponseEntityExceptionHandler 
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<String> handleAllUncaughtException(RuntimeException exception) {
+    public ResponseEntity<Object> handleAllUncaughtException(RuntimeException exception) {
         log.error("Unknown error occurred", exception);
         return new ResponseEntity<>("Unknown error occurred, please raise a support ticket!", HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 }
