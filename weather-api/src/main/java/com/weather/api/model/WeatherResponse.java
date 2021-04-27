0fe295a71f72d5a8106458d09398bd0fe67ce713
vivek22117 <vivekmishra22117@gmail.com>
2021-04-27 09:28:01 +0530

    adding response DTO for admin APIs

--- weather-api/src/main/java/com/weather/api/model/WeatherResponse.java
+++ weather-api/src/main/java/com/weather/api/model/WeatherResponse.java
@@ -3,11 +3,12 @@ package com.weather.api.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @Builder
-public class WeatherResponse {
+public class WeatherResponse implements Serializable {
 
     private WeatherDataDTO weatherDataDTO;
     private List<WeatherDataDTO> weatherDataDTOList;
