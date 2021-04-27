1ab38709dbcf825ab5fa9b92bef2c10ea8de9ca8
vivek22117 <vivekmishra22117@gmail.com>
2021-04-27 09:28:30 +0530

    Removing commented lines and unwatned imports

--- weather-api/src/main/java/com/weather/api/WeatherAPI.java
+++ weather-api/src/main/java/com/weather/api/WeatherAPI.java
@@ -1,16 +1,18 @@
 package com.weather.api;
 
+import com.weather.api.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+@Import(SwaggerConfig.class)
 public class WeatherAPI {
 
     public static void main(String[] args) {
         System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "local");
-//        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "dev");
 
         SpringApplication.run(WeatherAPI.class, args);
     }
