735365d4213d0b98056750e0e0cf470bde10d560
vivek22117 <vivekmishra22117@gmail.com>
2021-04-28 11:33:45 +0530

    Adding custom exception for Weather Not found

--- weather-api/src/main/java/com/weather/api/exception/WeatherDataNoFoundException.java
+++ weather-api/src/main/java/com/weather/api/exception/WeatherDataNoFoundException.java
@@ -3,5 +3,6 @@ package com.weather.api.exception;
 public class WeatherDataNoFoundException extends RuntimeException {
 
     public WeatherDataNoFoundException(String message) {
+        super(message);
     }
 }
