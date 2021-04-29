3d27800f896499d0906d06138d7fd4a860a2bf4b
vivek22117 <vivekmishra22117@gmail.com>
2021-04-29 08:13:00 +0530

    imports for component in a different file

---
+++ weather-ui/src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Login } from './Login/Login';
+export { default as Signup } from './Signup/Signup';
+export { default as Dashboard } from './Dashboard/Dashboard';
+export { default as SignInOutContainer } from '../container';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Home } from './Home/Home';
+export { default as AlertMsg } from './Alert/AlertMsg';
