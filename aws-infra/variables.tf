4ff98108c9aeed7f7bb8d6fc36d74844b380d127
vivek22117 <vivekmishra22117@gmail.com>
2021-05-13 00:03:08 +0530

    updating readme document

---
+++ aws-infra/variables.tf
@@ -0,0 +1,58 @@
+######################################################################
+# Global variables for VPC, Subnet, Routes and Bastion Host          #
+######################################################################
+variable "profile" {
+  type        = string
+  description = "AWS Profile name for credentials"
+}
+
+variable "default_region" {
+  type        = string
+  description = "AWS region to deploy resources"
+}
+
+######################################################
+# Local variables defined                            #
+######################################################
+variable "team" {
+  type        = string
+  description = "Owner team for this application infrastructure"
+}
+
+variable "owner" {
+  type        = string
+  description = "Owner of the product"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment to be configured 'dev', 'qa', 'prod'"
+}
+
+variable "isMonitoring" {
+  type        = bool
+  description = "Monitoring is enabled or disabled for the resources creating"
+}
+
+#####=============Local & Default Variables===============#####
+
+variable "dyanamoDB_prefix" {
+  type    = string
+  default = "doubledigit-tfstate"
+}
+
+variable "s3_bucket_prefix" {
+  type    = string
+  default = "doubledigit-tfstate"
+}
+
+
+locals {
+  common_tags = {
+    owner       = var.owner
+    team        = var.team
+    environment = var.environment
+    monitoring  = var.isMonitoring
+    Project     = "DD-Solutions"
+  }
+}
